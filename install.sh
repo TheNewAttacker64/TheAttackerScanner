14ce404df5af91e1c23895ed99fce7bd22cc3d1f
TheNewAttacker <57035255+TheNewAttacker64@users.noreply.github.com>
2023-04-04 16:33:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt install python3-pip
 pip install -r req.txt
 sudo cp theattacker.py /usr/local/bin/theattacker
-sudo chmod +x theattacker
+sudo chmod +x /usr/local/bin/theattacker
 echo  "[+] Just run python3 theattacker -h"
